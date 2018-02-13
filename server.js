17be9783e6bb64b1b3f61b381e9a947f8e1d6fe3
Adrian Rak <adrrak@tlen.pl>
2018-02-13 21:03:55 +0100

    todoApp

--- server.js
+++ server.js
@@ -16,13 +16,44 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://<adrrak>:<chopin>@ds233238.mlab.com:33238/database-1', {
     useMongoClient: true
 });
+// mongoose.connect('mongodb://localhost/nodeappdatabase', {
+//     useMongoClient: true
+// });
+
+const todoSchema = new Schema({
+    //username: { type: String, required: true},
+    list: { type: String, required: true }
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
 
 app.get('/', function(req, res) {
     res.render('index');
 });
 
-app.get('/todo', function (req, res) {
-    res.render('todo', { name: req.query.first_name });
+app.get('/add-todo', function(req, res) {
+    res.render('add-todo');
+});
+
+app.get('/todo', function(req, res) {
+    Todo.find()
+        .then(function(todo) {
+            res.render('todo', {data: todo});
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+});
+
+app.post('/addtodo', function(req, res) {
+    let newTodo = new Todo(req.body);
+    newTodo.save()
+        .then(function() {
+            res.redirect('todo');
+        })
+        .catch(function(err) {
+            console.log(err)
+        });
 });
 
 let server = app.listen(app.get('port'), function() {
