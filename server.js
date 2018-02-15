c82e0732fe5d92237f97a5937e79526ce4c06d38
Adrian Rak <adrrak@tlen.pl>
2018-02-15 15:24:54 +0100

    add task

--- server.js
+++ server.js
@@ -5,24 +5,22 @@ const app = express();
 const bodyParser = require('body-parser');
 
 app.use(express.static('./'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('port', (process.env.PORT || 5000));
 app.set('view engine', 'pug');
 app.set('views','./views');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<adrrak>:<chopin>@ds233238.mlab.com:33238/database-1', {
-    useMongoClient: true
-});
+mongoose.connect('mongodb://<adrrak>:<chopin>@ds233238.mlab.com:33238/database-1');
 // mongoose.connect('mongodb://localhost/nodeappdatabase', {
 //     useMongoClient: true
 // });
 
 const todoSchema = new Schema({
     //username: { type: String, required: true},
-    list: { type: String, required: true }
+    task: { type: String, required: true }
 });
 
 const Todo = mongoose.model('Todo', todoSchema);
@@ -30,12 +28,7 @@ const Todo = mongoose.model('Todo', todoSchema);
 app.get('/', function(req, res) {
     res.render('index');
 });
-
-app.get('/add-todo', function(req, res) {
-    res.render('add-todo');
-});
-
-app.get('/todo', function(req, res) {
+app.get('/todo', function (req, res) {
     Todo.find()
         .then(function(todo) {
             res.render('todo', {data: todo});
@@ -44,17 +37,36 @@ app.get('/todo', function(req, res) {
             console.log(err);
         });
 });
-
-app.post('/addtodo', function(req, res) {
-    let newTodo = new Todo(req.body);
+app.post('/addtask', function (req, res) {;
+    var newTodo = new Todo (req.body);
     newTodo.save()
         .then(function() {
             res.redirect('todo');
         })
         .catch(function(err) {
-            console.log(err)
+            console.log(err.message);
         });
 });
+// app.post('/addtask', function(req, res){
+//     let todoInfo = req.body;
+//     let newTodo = new Todo({
+//         task: todoInfo.task
+//     });
+//     newTodo.save()
+//         .then(function() {
+//             res.redirect('todo');
+//         })
+//         .catch(function(err) {
+//             console.log(err)
+//     });
+// });
+
+
+app.get('/todo', function(req, res) {
+    res.render('todo');
+});
+
+
 
 let server = app.listen(app.get('port'), function() {
     console.log('Node app is running on port ', app.get('port'));
